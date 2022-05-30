b65c22414b58087f1e79d74829baec1eb0b494e8
LegendeJJ <dalienjj@gmail.com>
2022-05-30 14:01:21 +0200

    Added small UI for the game.

--- app.js
+++ app.js
@@ -10,8 +10,103 @@ function computerPlay() {
     }
     console.log("Computer is "+sign);
     return sign;
-    
 }
+
+const message= document.querySelector(".message");
+const resetBtn= document.querySelector(".reset");
+const pscore= document.querySelector(".pscore");
+const cscore= document.querySelector(".cscore");
+let winner= "";
+let computerScore=0;
+let playerScore=0;
+let winningScore=5;
+let isGameOver=false;
+
+const rock= document.querySelector(".btnrock");
+rock.addEventListener("click", () =>{
+    playRound("Rock", computerPlay());
+    if(!isGameOver){
+          if(winner==="Player"){
+        playerScore++;
+        if(playerScore===winningScore){
+            message.innerHTML="<h3>Player Wins! Press Reset to start over!"
+            isGameOver=true;
+            rock.disabled="true";
+            paper.disabled="true";
+            scissor.disabled="true";
+        }
+        pscore.textContent=playerScore;
+    }else if(winner==="Computer"){
+        message.innerHTML="<h3>Computer Wins! Press Reset to start over!"
+        computerScore++;
+        if(computerScore===winningScore){
+            isGameOver=true;
+            rock.disabled="true";
+            paper.disabled="true";
+            scissor.disabled="true";
+        }
+        cscore.textContent=computerScore;
+    }
+    }
+  
+
+})
+const paper= document.querySelector(".btnpaper");
+paper.addEventListener("click", () =>{
+    playRound("Paper", computerPlay());
+    if(!isGameOver){
+        if(winner==="Player"){
+      playerScore++;
+      if(playerScore===winningScore){
+        message.innerHTML="<h3>Player Wins! Press Reset to start over!"
+          isGameOver=true;
+          rock.disabled="true";
+          paper.disabled="true";
+          scissor.disabled="true";
+      }
+      pscore.textContent=playerScore;
+  }else if(winner==="Computer"){
+      computerScore++;
+      if(computerScore===winningScore){
+        message.innerHTML="<h3>Computer Wins! Press Reset to start over!"
+          isGameOver=true;
+          rock.disabled="true";
+          paper.disabled="true";
+          scissor.disabled="true";
+      }
+      cscore.textContent=computerScore;
+  }
+  }
+})
+const scissor= document.querySelector(".btnscissor");
+scissor.addEventListener("click", () =>{
+    playRound("Scissors", computerPlay());
+    if(!isGameOver){
+        if(winner==="Player"){
+      playerScore++;
+      if(playerScore===winningScore){
+          message.innerHTML="<h3>Player Wins! Press Reset to start over!"
+          isGameOver=true;
+          rock.disabled="true";
+          paper.disabled="true";
+          scissor.disabled="true";
+      }
+      pscore.textContent=playerScore;
+  }else if(winner==="Computer"){
+      computerScore++;
+      if(computerScore===winningScore){
+        message.innerHTML="<h3>Computer Wins! Press Reset to start over!"
+          isGameOver=true;
+          rock.disabled="true";
+          paper.disabled="true";
+          scissor.disabled="true";
+      }
+      cscore.textContent=computerScore;
+  }
+  }
+})
+
+
 
 function playerPlay(){
     let userInput= prompt("Rock, Paper Scissors?");
@@ -22,66 +117,71 @@ function playerPlay(){
     return capitalized;
 }
 
+resetBtn.addEventListener("click", (e)=>{
+    isGameOver=false;
+    playerScore=0;
+    computerScore=0;
+    pscore.textContent=playerScore;
+    cscore.textContent=computerScore;
+    message.innerHTML="<h3>Press a Button</h3>";
+    rock.disabled=false;
+    paper.disabled=false;
+    scissor.disabled=false;
+})
+
 
 
 function playRound(playerSelection, computerSelection){ 
     let msg="";
-    let winner="";
-
-    
+    let winnerr="";
     if(playerSelection===computerSelection) {
         msg="Its a Draw!"
     }else if(playerSelection==="Rock"&&computerSelection=="Paper"){
         msg="You lose, Paper beats Rock!"
-        winner="Computer";
+        winnerr="Computer";
     }else if(playerSelection==="Rock"&&computerSelection=="Scissors"){
         msg="You win, Rock beats Scissors!"
-        winner="Player";
+        winnerr="Player";
     }else if(playerSelection==="Paper"&&computerSelection=="Rock"){
         msg="You win, Paper beats Rock!"
-        winner="Player";
+        winnerr="Player";
     }else if(playerSelection==="Paper"&&computerSelection=="Scissors"){
         msg="You lose, Scissors beats Paper!"
-        winner="Computer";
+        winnerr="Computer";
     }else if(playerSelection==="Scissors"&&computerSelection=="Paper"){
         msg="You win, Scissors beats Paper!"
-        winner="Player";
+        winnerr="Player";
     }else if(playerSelection==="Scissors"&&computerSelection=="Rock"){
         msg="You lose, Rock beats Scissors!"
-        winner="Computer";
+        winnerr="Computer";
     }
-    console.log(msg);
+    message.innerHTML="<h3>"+ msg+" Press another button.</h3>";
+    winner=winnerr;
     return winner;
 }
 
 
 function game(){
-   let computerscore=0;
-   let playerscore=0;
+
    let win="";
 
-   for (let i = 0; i < 5; i++) {    
+
     let currentWinner=playRound(playerPlay(), computerPlay());
 
     if(currentWinner==="Player"){
-        playerscore++;
+        playerScore++;
 
     }else if(currentWinner==="Computer"){
-        computerscore++;
-    }
-   }
-
-    if(playerscore>computerscore){
+        computerScore++;
+    } if(playerScore>computerScore){
         win="Player wins"
-    }else if(playerscore<computerscore){
+    }else if(playerScore<computerScore){
         win="Computer wins"
     }else{
         win="Its a draw, nobody wins"
     }
-    console.log("Player has a score of "+playerscore+" and Computer has a score of "+computerscore);
+    console.log("Player has a score of "+playerScore+" and Computer has a score of "+computerScore);
     console.log(win);
+   }
 
-     
-}
-
-game();
+   
